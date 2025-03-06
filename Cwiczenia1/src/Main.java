addef85a600b425b40b3ad8a1679f24133e8ef8b
Michal Pazio <elmiciu@gmail.com>
2025-03-06 11:13:13 +0100

    Zmiana 4

--- Cwiczenia1/src/Main.java
+++ Cwiczenia1/src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hell world!");
         int a = 4;
-        int b = 5;
+        int b = 2;
         int c = 3;
+        int d = 5;
     }
 }
